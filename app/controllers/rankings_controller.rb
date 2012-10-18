94c6ee47538c525a458aa4170d30eb7702c59634
Matt <mattmcdermott8@gmail.com>
2012-10-18 11:51:10 -0700

    Fixed small bug

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,12 +1,28 @@
 class RankingsController < ApplicationController
   def up
     @tutorial = Tutorial.find params[:tutorial_id]
+    unless (ranking = @tutorial.rankings.find_by_user_id(current_user.id)).nil?
+      if ranking.vote == 1
+        redirect_to root_path 
+        return
+      end
+      ranking.delete
+      @tutorial.update_attribute :cached_rating, (@tutorial.cached_rating + 1)
+    end
     @tutorial.rankings.create vote: 1, user_id: current_user.id
     redirect_to root_path
   end
 
   def down
     @tutorial = Tutorial.find params[:tutorial_id]
+    unless (ranking = @tutorial.rankings.find_by_user_id(current_user.id)).nil?
+      if ranking.vote == -1
+        redirect_to root_path 
+        return
+      end
+      ranking.delete
+      @tutorial.update_attribute :cached_rating, (@tutorial.cached_rating - 1)
+    end
     @tutorial.rankings.create vote: -1, user_id: current_user.id
     redirect_to root_path
   end
--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -7,6 +7,10 @@ class TutorialsController < ApplicationController
     @tutorial = Tutorial.find_by_id params[:id]
   end
 
+  def edit
+    @tutorial = Tutorial.find_by_id params[:id]
+  end
+
   def index
     @tutorials= Tutorial.all
   end
@@ -14,6 +18,21 @@ class TutorialsController < ApplicationController
   def create
     @tutorial = current_user.tutorials.build params[:tutorial]
     if @tutorial.save
+      redirect_to @tutorial
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @tutorial= Tutorial.find_by_id params[:id]
+    @tutorial.delete
+    redirect_to root_path
+  end
+
+  def update
+    @tutorial = Tutorial.find_by_id params[:id]
+    if @tutorial.update_attributes params[:tutorial]
       redirect_to @tutorial
     else
       render 'new'
